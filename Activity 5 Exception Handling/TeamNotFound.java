7e85c951cd1116bebf16ad5c4563b165f0a8feeb
99003678 <78944936+99003678@users.noreply.github.com>
2021-03-03 13:32:17 +0530

    Add files via upload

---
+++ Activity 5 Exception Handling/TeamNotFound.java
@@ -0,0 +1,53 @@
+
+import java.util.*;
+class Invalid extends Exception
+{  
+
+private static final long serialVersionUID = 1L;
+
+Invalid()
+     {
+    	 System.out.println("TeamNameNotFoundException: Entered team is not a part of IPL Season 13");
+    	 System.exit(0);
+     }
+}
+
+
+public class TeamNotFound {
+
+	public static void main(String[] args) throws Invalid{
+
+		 try (Scanner s = new Scanner(System.in)) {
+			String[] st = {"Chennai Super Kings","Rajasthan Royals" ,"Royal Challengers Bangalore"," Kings XI Punjab", "Kolkata Knight Riders", "Mumbai Indians"};
+			 System.out.println("Enter the expected winner team of IPL Season 13");
+			 String str=s.nextLine();
+			  for(String i:st)
+				      {
+				         if(i.equals(str))
+				            {
+				        	 
+				        	 System.out.println("Enter the expected runner team of IPL Season 13");
+				    		 String str1=s.nextLine();
+				    	  	  for (String ii:st)
+						      {
+						         if(ii.equals(str1))
+						            {
+						        	 System.out.println(" expected winner team of IPL Season 13:Rajasthan Royals");
+						        	 System.out.println(" expected Runner team of IPL Season 13:Kings XI Punjab");
+						            }
+						         else
+						        	 throw new Invalid();
+						       }
+				            }
+				         else
+				        	 throw new Invalid();
+				      }
+
+		}
+		
+	}
+		
+
+	}
+
+
